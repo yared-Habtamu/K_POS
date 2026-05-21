9b2289d9a8fe65bb2f872b589326194f5a66ff5c
Tesfamichael12 <tesfamichael132@gmail.com>
2026-05-21 09:54:34 +0300

    feat: add PWA support with service worker, manifest, and desktop entry

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,8 +3,14 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch((error) => {
+    console.error("Service worker registration failed:", error);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
-	<ThemeProvider>
-		<App />
-	</ThemeProvider>,
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
 );
